200deb8adb8143eb85b6c0cf4be89b233ef8c405
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2024-04-14 23:39:50 +0200

    Add project files.

---
+++ EAFC Discord bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EAFC_Discord_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
